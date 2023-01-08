32e336bcc2ae5435b1be1791afde1394fa0eaa93
sergey <sergio44k@gmail.com>
2023-01-08 22:39:28 +0200

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Database from "./components/Database";
 import Form from "./components/Form";
 import ListOfExpenses from "./components/ListOfExpenses";
-import Database from "./components/Database";
 
 const App = () => {
+  const [expenses, setExpenses] = useState(Database.getExpenses());
+
   return (
     <div>
       <h1>Expenses</h1>
-      <Form />
-      <ListOfExpenses />
+      <Form setExpenses={setExpenses} />
+      <ListOfExpenses expenses={expenses} setExpenses={setExpenses} />
     </div>
   );
 };
-
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,70 @@
-import React, { useState } from 'react';
-import Database from './Database'; // Import the Database component
+import React, { useState } from "react";
+import Database from "./Database"; // Import the Database component
 
-const Form = () => {
+const Form = ({ setExpenses }) => {
   // State for the form input values
   const [formData, setFormData] = useState({
     //time will be timestapm
-    cost: '',
-    name: '',
-    description: '',
-    time: (new Date()).getTime()
+    cost: "",
+    name: "",
+    description: "",
+    time: new Date().getTime(),
   });
 
   // Function to handle input changes
   const handleChange = (event) => {
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   // Function to handle form submission
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(formData);
     // Add the new expense to the database
     Database.addExpense(formData);
+    setExpenses((prevExpenses) => [...prevExpenses, formData]);
     // Reset the form
     setFormData({
-      cost: '',
-      name: '',
-      description: '',
-      time: (new Date()).getTime()
+      cost: "",
+      name: "",
+      description: "",
+      time: new Date().getTime(),
     });
     const temp = Database.getExpenses();
-    console.log('----------');
+    console.log("----------");
     console.log(temp);
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="cost">Cost:</label>
-      <input type="number" name="cost" value={formData.cost} onChange={handleChange} />
+      <input
+        type="number"
+        name="cost"
+        value={formData.cost}
+        onChange={handleChange}
+      />
       <br />
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
       <br />
       <label htmlFor="description">Description:</label>
-      <input type="text" name="description" value={formData.description} onChange={handleChange} />
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+      />
       <br />
-      <button type="button" onClick={handleSubmit}>Submit</button>
+      <button type="submit">Submit</button>
     </form>
   );
 };
